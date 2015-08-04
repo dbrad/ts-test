88a65abd37093967657d322df4c70378c2262f62
David Brad <davidbrad.015@gmail.com>
2015-08-04 16:56:13 -0400

    Playing with phaser

--- src/game.ts
+++ src/game.ts
@@ -4,35 +4,55 @@ class Game extends Phaser.State {
     this.game = new Phaser.Game(800,600,Phaser.AUTO, "content", this);
   }
   game: Phaser.Game;
-  text: Phaser.Text;
+  sprites: Phaser.Sprite[] = [];
+
 
   preload() {
     console.log("preload");
+
   }
 
   create() {
     console.log("create");
-    this.text = this.game.add.text(400, 300, "HEY", {fill: "white"});
-    this.text.anchor.setTo(0.5, 0.5);
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    var graphics = this.game.add.graphics(0,0);
+    graphics.beginFill(0xFFFFFF);
+    graphics.drawRect(0,0,16,16);
+    graphics.endFill();
+    graphics.visible = false;
+    var texture = graphics.generateTexture();
+
+    var counter:number = 0;
+
+    while(counter < 10) {
+      this.sprites.push(this.game.add.sprite(32*counter,32*counter,texture));
+      var sprite = this.sprites[counter];
+
+      sprite.anchor.setTo(0.5,0.5);
+      this.game.physics.enable(sprite, Phaser.Physics.ARCADE);
+
+      sprite.body.velocity.setTo(200,200);
+      sprite.body.collideWorldBounds = true;
+
+      sprite.body.bounce.set(1);
+      counter++;
+    }
+
+    this.game.physics.arcade.gravity.y = 225;
   }
 
   update() {
-    this.text.rotation += 0.01;
-    if(this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
-      this.text.rotation += 0.02;
-    }
+    // this.game.physics.arcade.velocityFromAngle(
+    //   this.sprite.angle, 400, this.sprite.body.velocity);
+
   }
 }
+
+
 
 window.onload = () => {
 
     var game = new Game();
 
 };
-
-class Menu extends Phaser.State {
-  constructor() {
-    super();
-  }
-
-}
